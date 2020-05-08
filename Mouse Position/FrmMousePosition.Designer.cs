f50e67db4b76daaa3e3f3e7472739426ecd6db98
Enaldo Santos <enaldosantos94@gmail.com>
2020-05-08 15:25:46 -0300

    Aprendendo

---
+++ Mouse Position/FrmMousePosition.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Mouse_Position
+{
+    partial class FrmMousePosition
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMousePosition));
+            this.LblMousePosition = new System.Windows.Forms.Label();
+            this.timerMousePosition = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LblMousePosition
+            // 
+            this.LblMousePosition.AutoSize = true;
+            this.LblMousePosition.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblMousePosition.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.LblMousePosition.Location = new System.Drawing.Point(12, 17);
+            this.LblMousePosition.Name = "LblMousePosition";
+            this.LblMousePosition.Size = new System.Drawing.Size(185, 29);
+            this.LblMousePosition.TabIndex = 0;
+            this.LblMousePosition.Text = "Mouse Position:";
+            // 
+            // timerMousePosition
+            // 
+            this.timerMousePosition.Enabled = true;
+            this.timerMousePosition.Tick += new System.EventHandler(this.timerMousePosition_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.label1.Location = new System.Drawing.Point(282, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Help: h";
+            // 
+            // FrmMousePosition
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(320, 63);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LblMousePosition);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMousePosition";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MousePosition";
+            this.TopMost = true;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FrmMousePosition_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblMousePosition;
+        private System.Windows.Forms.Timer timerMousePosition;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Mouse Position/FrmMousePosition.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Mouse_Position
+{
+    public partial class FrmMousePosition : Form
+    {
+        public FrmMousePosition()
+        {
+            InitializeComponent();
+        }
+
+        int positionX;
+        int positionY;
+        private void timerMousePosition_Tick(object sender, EventArgs e)
+        {
+            positionX = MousePosition.X;
+            positionY = MousePosition.Y;
+
+            LblMousePosition.Text = $"Mouse Position: {positionX} x {positionY}";
+
+            if(BackColor == Color.DarkGreen)
+                BackColor = Color.FromArgb(20, 20, 20);
+        }
+        private void FrmMousePosition_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                Close();
+            }
+            else if(e.KeyCode == Keys.Tab)
+            {
+                Clipboard.SetText(LblMousePosition.Text);
+                BackColor = Color.DarkGreen;
+            }
+            else if(e.KeyCode == Keys.H)
+            {
+                MessageBox.Show("Welcome to Mouse Position\n\n" +
+                                "[+] ESC - Close app\n" +
+                                "[+] TAB - Copy Position\n" +
+                                "[+] Version: 1.0\n\n" +
+                                "Naldo Santos",
+                                "Mouse Position - HELP", // Title MessageBox
+                                 MessageBoxButtons.OK,
+                                 MessageBoxIcon.Information);
+            }
+        }
+    }
+}
